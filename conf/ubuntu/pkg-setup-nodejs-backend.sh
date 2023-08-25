e57dc886090307772d2fb76d675a98012e09b591
Arthur de Rugy <117949192+aderugy@users.noreply.github.com>
2023-08-25 20:09:32 +0200

    Add nodemon

--- conf/ubuntu/pkg-setup-nodejs-backend.sh
+++ conf/ubuntu/pkg-setup-nodejs-backend.sh
@@ -9,10 +9,11 @@ sudo apt -y install ca-certificates curl gnupg
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-source .bashrc
+source "$HOME/.bashrc"
 
 sudo nvm install --lts
 sudo apt -y install npm
+npm i -g nodemon
 
 # MySQL
 sudo apt -y install mysql-server
